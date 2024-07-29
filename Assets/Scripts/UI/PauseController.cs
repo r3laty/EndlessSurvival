cdf3a1894d81042dd0b0d1e9d03853d7a4f47e61
Ilya Molchanov <lolkers.ilya@gmail.com>
2024-07-29 13:33:11 +0300

    Make volume controller

--- Assets/Scripts/UI/PauseController.cs
+++ Assets/Scripts/UI/PauseController.cs
@@ -1,12 +1,21 @@
-using System.Collections;
 using UnityEngine;
+using Zenject;
 
 public class PauseController : MonoBehaviour
 {
     [SerializeField] private Canvas pauseMenu;
+    [Space]
+    [SerializeField] private GameObject volumeControlMenu;
+
+    private GameOverController _gameOverController;
 
     private bool _pauseButton;
     private bool _isPaused;
+    private void Awake()
+    {
+        _gameOverController = GetComponent<GameOverController>();
+    }
+    #region Pause
     public void SetPauseButton(bool pauseButton)
     {
         _pauseButton = pauseButton;
@@ -29,6 +38,7 @@ public class PauseController : MonoBehaviour
     }
     private void PauseGame()
     {
+        volumeControlMenu.gameObject.SetActive(false);
         Time.timeScale = 0;
         pauseMenu.gameObject.SetActive(true);
         _isPaused = true;
@@ -38,5 +48,45 @@ public class PauseController : MonoBehaviour
         Time.timeScale = 1;
         pauseMenu.gameObject.SetActive(false);
         _isPaused = false;
+    }
+    #endregion
+    /// <summary>
+    /// Method on button
+    /// </summary>
+    public void OnPauseButton()
+    {
+        PauseGame();
+    }
+
+    /// <summary>
+    /// Method on button
+    /// </summary>
+    public void OnContinueButton()
+    {
+        ResumeGame();
+    }
+
+    /// <summary>
+    /// Method on button
+    /// </summary>
+    public void OnTryAgainButton()
+    {
+        _gameOverController.ResetAll();
+    }
+
+    /// <summary>
+    /// Method on button
+    /// </summary>
+    public void OnLeaveButton()
+    {
+        
+    }
+
+    /// <summary>
+    /// Method on button
+    /// </summary>
+    public void OnVolumeControlButton()
+    {
+        volumeControlMenu.gameObject.SetActive(true);
     }
 }
---
+++ Assets/Scripts/UI/VolumeController.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VolumeController : MonoBehaviour
+{
+    [SerializeField] private Slider shotSoundSlider;
+    [SerializeField] private Slider boostSoundSlider;
+
+    private void Update()
+    {
+        AudioManager.Instance.ShotVolume = shotSoundSlider.value;
+        AudioManager.Instance.BoosterVolume = boostSoundSlider.value;
+    }
+}
